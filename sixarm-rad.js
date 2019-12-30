4baabeceb7b11aa053b9066c6c5bed212a5ffd0e
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-12-29 22:55:55 -0800

    Add parseQueryString

--- sixarm-rad.js
+++ sixarm-rad.js
@@ -125,8 +125,42 @@ function elementsByClassName(className) {
 //
 ////
 
-// Parse a URL string to its parameters, such as query string key-value items.
-// See https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+/**
+ * Parse a query string to its parameters.
+ * See https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+ */
+function parseQueryString(queryString) {
+    let pairs = queryString.split('&'));
+    if (pairs == "") return {};
+    var params = {};
+    for (var i = 0; i < pairs.length; ++i)
+    {
+        var p = pairs[i].split('=', 2);
+        if (p.length == 1)
+            params[p[0]] = "";
+        else
+            params[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return params;
+}
+
+/**
+ * Parse a URL string to its parameters, such as query string key-value items.
+ * See https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+ *
+ * The code is thanks to Google. It is obfuscated, but it is understandable. 
+ * It does not work because some variables are undefined.
+ * 
+ * They start to look for parameters on the url from ? and also from the hash #. 
+ * Then for each parameter they split in the equal sign b[f][p]("=") (which looks 
+ * like indexOf, they use the position of the char to get the key/value). 
+ *
+ * Having it split they check whether the parameter has a value or not, if 
+ * it has then they store the value of d, otherwise they just continue.
+ *
+ * In the end the object d is returned, handling escaping and the + sign. 
+ * This object is just like mine, it has the same behavior.
+ */
 function getURLParameters(b) {
     var c = typeof b === "undefined";
     if (a !== h && c) return a;
